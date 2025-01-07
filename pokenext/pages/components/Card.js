38ce656c21066f5391a70c67d66ce33fedd2d018
Kayo Fernando Lima Silva <kaio16.fernando@gmail.com>
2025-01-06 23:06:29 -0300

    finalizando o componente do card

--- pokenext/pages/components/Card.js
+++ pokenext/pages/components/Card.js
@@ -6,12 +6,12 @@ export default function Card({pokemon}){
     
     return(
         <div className={styles.card}>
-            <Image
-                src={`https://img.pokemondb.net/sprites/home/normal/${pokemon.name}.png`}
-                height="120"
-                width="120"
-                alt={pokemon.name}
-            />
+                <Image
+                    src={`https://img.pokemondb.net/sprites/home/normal/${pokemon.name}.png`}
+                    height="120"
+                    width="120"
+                    alt={pokemon.name}
+                />
             <p className={styles.id}>#{pokemon.id}</p>
             <h3 className={styles.description}>{pokemon.name}</h3>
             <Link href={`/pokemon/${pokemon.id}`} legacyBehavior>
